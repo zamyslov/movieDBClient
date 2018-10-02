57b87dcd7b723d9eda2783e7733fb037a755bf99
zamyslov@gmail.com <194156567>
2018-10-02 12:26:36 +0300

    add login-page & register-page components with routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
+import {AuthLayoutComponent} from "./shared/layouts/auth-layout/auth-layout.component";
+import {SiteLayoutComponent} from "./shared/layouts/site-layout/site-layout.component";
 import {LoginPageComponent} from "./login-page/login-page.component";
+import {RegisterPageComponent} from "./register-page/register-page.component";
 
 const routes: Routes = [
-  {path: 'login', component: LoginPageComponent}
+  {path: '', component: AuthLayoutComponent, children: [
+      {path: '', redirectTo: '/login', pathMatch: 'full'},
+      {path: 'login', component: LoginPageComponent},
+      {path: 'registration', component: RegisterPageComponent}
+    ]},
+  {path: '', component: SiteLayoutComponent, children: [
+
+    ]}
 ];
 
 @NgModule({
