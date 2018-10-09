c0fc32d240417d5b0bcc4176b1dfbf2d462a2e3a
zamyslov@gmail.com <194156567>
2018-10-09 17:48:19 +0300

    add image upload in movies-add component

--- client/src/app/movies-page/movies-add/movies-add.component.ts
+++ client/src/app/movies-page/movies-add/movies-add.component.ts
@@ -1,5 +1,6 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {MaterialService} from "../../shared/classes/material.service";
+import {FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-movies-add',
@@ -9,14 +10,40 @@ import {MaterialService} from "../../shared/classes/material.service";
 export class MoviesAddComponent implements OnInit, AfterViewInit {
 
   @ViewChild('select') selectRef: ElementRef;
+  @ViewChild('input') inputRef: ElementRef;
+  form: FormGroup;
+  image: File;
+  imagePreview = '';
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    this.form = new FormGroup({});
   }
 
   ngAfterViewInit() {
     MaterialService.initializeMultiSelect(this.selectRef);
+  }
+
+  triggerClick() {
+    this.inputRef.nativeElement.click();
+  }
+
+  onFileUpload(event: any) {
+    const file = event.target.files[0];
+    this.image = file;
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    };
+    reader.readAsDataURL(file);
+
+  }
+
+  onSubmit() {
+
   }
 
 }
